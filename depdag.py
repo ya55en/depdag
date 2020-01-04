942f0171e4675daa6e2b0170ce5a064880b92454
Yassen Damyanov <yassen.damyanov@tide.co>
2020-01-04 15:10:17 +0200

    Eliminate VerticesMap class (#6) and more improvements
    - Remove VerticesMap class and transfer all methods to DepDag (#6);
    - Add DepDag.__call__ method to catch misspelled method calls;
    - Remove documentation from module docstring; refer to the README;
    - Bump version to 0.3.1 (development).

--- depdag.py
+++ depdag.py
@@ -1,63 +1,25 @@
 # Distributed under the MIT license.
-# Copyright (c) 2020 Yassen Damyanov <yassen.damyanov.bg -AT- gmail.com>
-# See LICENSE file for details.
+# Copyright (c) 2020 Yassen Damyanov and contributors
+# See LICENSE and CREDITS for details.
 
 """
 DAG-based dependency tracking utility.
 
-Helps track dependencies via representing dependency relationships as edges in
-a Directed Acyclic Graph (DAG). We call a vertex depending on other vertices
-a *dependant*, and a vertex that other vertices depend on a *supporter*.
+Helps track dependencies via representing dependency relationships
+as edges in a Directed Acyclic Graph (DAG).
 
-In case entities represented by DAG vertices may have certain payload associated
-with them, depdag provides answer to the question: "Is this vertex resolved?",
-that is, has it and all its supporters, recursively, being provided with payload
--- the `Vertex::is_resolved()` method does that.
-
-Here an example of typical usage::
-
- from depdag import DepDag
-
- # Create the DAG structure and get a reference to the vertices collection:
- vert = DepDag().vertices
-
- # Connect vertices with directed dependency relationships (i.e. the edges):
- vert.a.depends_on('b')
- vert.b.depends_on('d')
- vert.c.depends_on('d', 'e')
- assert not vert.dag.is_cyclic()
-
- # Explore who depends on whom, recursively; prints:
- # - vert a -> all supporters: ['b', 'd']
- # - vert b -> all supporters: ['d']
- # - vert d -> all supporters: []
- # - vert c -> all supporters: ['d', 'e']
- # - vert e -> all supporters: []
- for v in vert.all():
-     print("- vert", v.name, "-> all supporters:", v.supporters(recurse=True))
-
- # Set some payload and see which vertices are 'resolved', that is,
- # all supporters, recursively, also have payload and the vertex
- # itself has a payload
-
- vert.a.payload = "vert-a payload (can be anything)"
- vert.d.payload = "vert-d payload (can be anything)"
- assert not vert.a.is_resolved()
- assert vert.d.is_resolved()
- assert not vert.c.is_resolved()
- vert.c.payload = "vert-c payload (can be anything)"
- vert.e.payload = "vert-d payload (can be anything)"
- assert vert.c.is_resolved()
+For details and usage example see ``depdag`` README.rst:
+  https://github.com/yassen-itlabs/depdag/blob/master/README.rst
 
 """
 
 from __future__ import annotations
 
-__version_tuple__ = (0, 2, 1)
+__version_tuple__ = (0, 3, 1)
 __version__ = '.'.join(map(str, __version_tuple__))
 
 from collections import OrderedDict
-from typing import List, Dict, Iterable, Optional, Any, Hashable
+from typing import List, Dict, Iterable, Hashable, Any
 
 VertexName = Hashable
 
@@ -68,11 +30,17 @@ class Vertex:
     and its provision state (provided or not).
     """
 
-    def __init__(self, name: VertexName, vertices_map: VerticesMap):
+    def __init__(self, name: VertexName, vertices_map: DepDag, payload: Any = None):
         self._name: VertexName = name
-        self._vertices_map: VerticesMap = vertices_map
+        self._vertices_map: DepDag = vertices_map
         self._supporters: OrderedDict = OrderedDict()
-        self.payload: Any = None
+        self.payload: Any = payload
+
+    def __call__(self, *args, **kwargs):
+        """Provide proper error in case a misspelled ``DepDag`` method is called.
+        Without this one, one gets "TypeError: 'Vertex' object is not callable.
+        """
+        raise AttributeError(f"Vertex object has no attribute {self._name!r}")
 
     def __repr__(self) -> str:
         return f"<Vertex(name={self._name!r}) object at 0x{id(self):x})>"
@@ -94,7 +62,8 @@ class Vertex:
         if recurse:
             # gather all supporters, recursively:
             return list(self._supporters.keys()) + [
-                name for vert in self._supporters.values()
+                name
+                for vert in self._supporters.values()
                 for name in vert.supporters(recurse=True)
             ]
         # return direct supporters of this vertex only:
@@ -106,25 +75,22 @@ class Vertex:
         )
 
 
-class VerticesMap:
-    """A dict-like collection of vertices. Maps vertex names to corresponding
-    vertices. Creates a vertex under given name on first access (if not there
-    yet).
+class DepDag:
+    """DAG based dependency tracking main class.
+
+    A dict-like collection of Vertices. Maps vertex names to corresponding
+    ``Vertex`` objects. Creates a ``Vertex`` under given name on first access
+    (if not there yet).
 
     Will not accept ``__setattr__`` or ``__setitem__`` assignments for vertex
     creation -- just access the not-yet-created vertex and you have it, or
     use the ``create()`` method.
     """
 
-    __slots__ = ('_vertices', '_dag')
+    __slots__ = ('_vertices',)
 
-    def __init__(self, dag: Optional[DepDag]):
+    def __init__(self):
         self._vertices: Dict[VertexName, Vertex] = dict()
-        self._dag = dag
-
-    @property
-    def dag(self):
-        return self._dag
 
     def __contains__(self, item):
         return item in self._vertices
@@ -153,24 +119,13 @@ class VerticesMap:
     def all(self) -> Iterable[Vertex]:
         return self._vertices.values()
 
-
-class DepDag:
-    """DAG based dependency tracking utility class."""
-
-    def __init__(self):
-        self._vertices: VerticesMap = VerticesMap(self)
-
-    @property
-    def vertices(self) -> VerticesMap:
-        return self._vertices
-
     def is_cyclic(self) -> bool:
         """Return True if this directed graph contains at least one cycle,
         False otherwise."""
-        # Yes -- ugly, Q&D, expensive implementation. A better one is on the way ;)
+        # Yes -- ugly, Q&D implementation. A better one is on the way ;)
         # TODO: provide proper implementation
         try:
-            for vert in self._vertices.all():
+            for vert in self._vertices.values():
                 vert.supporters(recurse=True)
         except RecursionError:
             return True
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -3,50 +3,56 @@ depdag classes unit tests.
 """
 
 import unittest
-from depdag import Vertex, VerticesMap, DepDag
+from depdag import Vertex, DepDag
 
 
 class TestVertex(unittest.TestCase):
 
     def test_creation__test_name(self):
-        vertex = Vertex('vertex_11', VerticesMap(None))
+        vertex = Vertex('vertex_11', DepDag())
         self.assertEqual('vertex_11', vertex.name)
-        self.assertFalse(vertex.provided)
+        self.assertFalse(vertex.is_provided)
 
     def test__repr__(self):
-        vtx = Vertex('vertex_22', VerticesMap(None))
+        vertex = Vertex('vertex_22', DepDag())
         expected = "<Vertex(name='vertex_22') object at 0x"
-        self.assertTrue(repr(vtx).startswith(expected))
+        self.assertTrue(repr(vertex).startswith(expected))
+
+    def test__contains__(self):
+        dag = DepDag()
+        dag.one.depends_on('two')
+        self.assertTrue('one' in dag)
+        self.assertTrue('two' in dag)
 
     def test_provide_payload(self):
-        vertex = Vertex('vertex_33', VerticesMap(None))
-        self.assertFalse(vertex.provided)
+        vertex = Vertex('vertex_33', DepDag())
+        self.assertFalse(vertex.is_provided)
         vertex.payload = "any payload would do"
-        self.assertTrue(vertex.provided)
+        self.assertTrue(vertex.is_provided)
 
     def test_depends_on__test_supporters(self):
-        vtx = Vertex('vertex_0', VerticesMap(None))
-        vtx.depends_on('vertex_1', 'vertex_2')
+        vertex = Vertex('vertex_0', DepDag())
+        vertex.depends_on('vertex_1', 'vertex_2')
         expected = ['vertex_1', 'vertex_2']
-        self.assertEqual(expected, vtx.supporters(recurse=False))
-        vtx.depends_on('vertex_2', 'vertex_3')
+        self.assertEqual(expected, vertex.supporters(recurse=False))
+        vertex.depends_on('vertex_2', 'vertex_3')
         expected = ['vertex_1', 'vertex_2', 'vertex_3']
-        self.assertEqual(expected, vtx.supporters(recurse=False))
+        self.assertEqual(expected, vertex.supporters(recurse=False))
 
     def test_all_supporters(self):
-        vmap = VerticesMap(None)
-        a = Vertex('a', vmap)
+        dag = DepDag()
+        a = Vertex('a', dag)
         a.depends_on('b')
-        b = vmap['b']
+        b = dag['b']
         b.depends_on('c')
         self.assertEqual(['b', 'c'], a.supporters(recurse=True))
 
     def test_is_resolved__simplest_case(self):
-        vmap = VerticesMap(None)
-        a = Vertex('a', vmap)
+        dag = DepDag()
+        a = Vertex('a', dag)
         a.depends_on('b')
         self.assertFalse(a.is_resolved())
-        b = vmap['b']
+        b = dag['b']
         self.assertFalse(b.is_resolved())
         b.payload = 'payload for vert-b'
         self.assertFalse(a.is_resolved())
@@ -57,69 +63,70 @@ class TestVertex(unittest.TestCase):
 
     def test_vertex_name_is_tuple(self):
         # exercising any hashable to be used as a vertex name (PR #2)
-        vmap = VerticesMap(None)
-        vmap[('vertex_a',)].depends_on(('vertex_b',))
+        dag = DepDag()
+        dag[('vertex_a',)].depends_on(('vertex_b',))
         self.assertEqual(
             [('vertex_b',)],
-            vmap[('vertex_a',)].supporters(recurse=True)
+            dag[('vertex_a',)].supporters(recurse=True)
         )
         self.assertEqual(
             [],
-            vmap[('vertex_b',)].supporters(recurse=True)
+            dag[('vertex_b',)].supporters(recurse=True)
         )
-
-    def test__contains__(self):
-        vmap = VerticesMap(None)
-        vmap.one.depends_on('two')
-        self.assertTrue('one' in vmap)
-        self.assertTrue('two' in vmap)
-
-
-class TestVerticesMap(unittest.TestCase):
-
-    def test_creation(self):
-        VerticesMap(None)
-
-    def test__getitem__(self):
-        vert = VerticesMap(None)
-        vertex_a = vert['a']
-        self.assertIsInstance(vertex_a, Vertex)
-        self.assertEqual('a', vertex_a.name)
-
-    def test__setitem__(self):
-        vert = VerticesMap(None)
-        with self.assertRaises(NotImplementedError):
-            vert['whatever'] = Vertex('whatever', vert)
-
-    def test__getattr__(self):
-        vert = VerticesMap(None)
-        vertex = vert.vertex_384
-        self.assertIsInstance(vertex, Vertex)
-        self.assertEqual('vertex_384', vertex.name)
-
-    def test__setattr__(self):
-        vert = VerticesMap(None)
-        with self.assertRaises(AttributeError):
-            vert.z = Vertex('z', vert)
 
 
 class TestDag(unittest.TestCase):
 
     def test_creation(self):
         dag = DepDag()
+        self.assertEqual(0, len(dag))
+
+    def test__getitem__(self):
+        dag = DepDag()
+        vertex_a = dag['a']
+        self.assertIsInstance(vertex_a, Vertex)
+        self.assertEqual('a', vertex_a.name)
+
+    def test__setitem__(self):
+        dag = DepDag()
+        with self.assertRaises(NotImplementedError):
+            dag['whatever'] = Vertex('whatever', dag)
+
+    def test__getattr__(self):
+        dag = DepDag()
+        vertex = dag.vertex_384
+        self.assertIsInstance(vertex, Vertex)
+        self.assertEqual('vertex_384', vertex.name)
+
+    def test__setattr__(self):
+        dag = DepDag()
+        with self.assertRaises(AttributeError):
+            dag.whatever = Vertex('whatever', dag)
+
+    def test__call__(self):
+        dag = DepDag()
+        with self.assertRaisesRegex(
+                AttributeError, "Vertex object has no attribute 'misspelled'"):
+            dag.misspelled()
 
     def test_depends_on__test_supporters(self):
-        vert = DepDag().vertices
-        vert.a.depends_on('b')
-        self.assertEqual(['b'], vert.a.supporters(recurse=False))
-        vert.a.depends_on('c', 'd')
-        self.assertEqual(['b', 'c', 'd'], vert.a.supporters(recurse=False))
+        dag = DepDag()
+        dag.a.depends_on('b')
+        self.assertEqual(['b'], dag.a.supporters(recurse=False))
+        dag.a.depends_on('c', 'd')
+        self.assertEqual(['b', 'c', 'd'], dag.a.supporters(recurse=False))
 
     def test_all_supporters(self):
-        vert = DepDag().vertices
-        vert.a.depends_on('b')
-        vert.b.depends_on('c')
-        self.assertEqual(['b', 'c'], vert.a.supporters(recurse=True))
+        dag = DepDag()
+        dag.a.depends_on('b')
+        dag.b.depends_on('c')
+        self.assertEqual(['b', 'c'], dag.a.supporters(recurse=True))
+
+    def test_all(self):
+        dag = DepDag()
+        dag.a.depends_on('b')
+        dag.b.depends_on('c')
+        self.assertEqual([dag.a, dag.b, dag.c], list(dag.all()))
 
     def test_is_cyclic__negative__empty_dag(self):
         dag = DepDag()
@@ -127,30 +134,26 @@ class TestDag(unittest.TestCase):
 
     def test_is_cyclic__negative__one_vertex(self):
         dag = DepDag()
-        vert = dag.vertices
-        vert.create('a')
-        self.assertEqual(1, len(vert))
+        dag.create('a')
+        self.assertEqual(1, len(dag))
         self.assertFalse(dag.is_cyclic())
 
     def test_is_cyclic__negative_three_vertices(self):
         dag = DepDag()
-        vert = dag.vertices
-        vert.a.depends_on('b')
-        vert.b.depends_on('c')
+        dag.a.depends_on('b')
+        dag.b.depends_on('c')
         self.assertFalse(dag.is_cyclic())
 
     def test_is_cyclic__positive__one_vertex(self):
         dag = DepDag()
-        vert = dag.vertices
-        vert.a.depends_on('a')
+        dag.a.depends_on('a')
         self.assertTrue(dag.is_cyclic())
 
     def test_is_cyclic__positive_three_vertices(self):
         dag = DepDag()
-        vert = dag.vertices
-        vert.a.depends_on('b')
-        vert.b.depends_on('c')
-        vert.c.depends_on('a')
+        dag.a.depends_on('b')
+        dag.b.depends_on('c')
+        dag.c.depends_on('a')
         self.assertTrue(dag.is_cyclic())
 
 
